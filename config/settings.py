2226b090437e936734336ec4ba5009d0c75dcd50
esi <esi.taheri@yahoo.com>
2023-07-18 06:49:07 -0700

    add custom user model

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # local
     'pages',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -91,6 +92,8 @@ DATABASES = {
     }
 }
 
+# auth
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
